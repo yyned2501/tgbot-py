6cf9cf6559fcaac1ad885b5b72e29f769f4bfac2
DonneChang <luckydays.donne@gmail.com>
2025-05-02 09:21:45 +0800

    更新log

--- libs/log.py
+++ libs/log.py
@@ -5,6 +5,7 @@ import logging
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
+"""
 # 可选：东八区时间格式
 class CSTFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
@@ -12,7 +13,11 @@ class CSTFormatter(logging.Formatter):
         return time_utc8.strftime(datefmt or '%Y-%m-%d %H:%M:%S %Z')
 
 formatter = CSTFormatter("[%(levelname)s] %(asctime)s - %(filename)s - %(message)s")
+"""
 
+
+
+formatter = logging.Formatter("[%(levelname)s] %(asctime)s - %(filename)s - %(message)s")
 logger = logging.getLogger("Mytgbot")
 logger.setLevel(logging.INFO)
 
@@ -22,8 +27,7 @@ if not logger.handlers:
     file_handler = RotatingFileHandler("logs/Mytgbot.log", maxBytes=10 * 1024 * 1024, backupCount=10, encoding="utf-8")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
-
-    # 控制台输出，强制使用 UTF-8 编码1
+    # 控制台输出，强制使用 UTF-8 编码
     utf8_stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
     console_handler = logging.StreamHandler(utf8_stdout)
     console_handler.setFormatter(formatter)
