4946135b2856680577bd23253b3abcb77b4c2200
DonneChang <luckydays.donne@gmail.com>
2025-05-15 11:08:51 +0800

    更新

--- libs/mysql_backup.py
+++ libs/mysql_backup.py
@@ -12,7 +12,7 @@ BACKUP_DIR = Path("db_file/mysqlBackup")
 RETENTION_DAYS = 8  # 备份保留天数
 
 
-@scheduler.scheduled_job("cron",hour=11, minute=8, id="zsss")
+@scheduler.scheduled_job("cron",hour=11, minute=10, id="zsss")
 async def mysql_backup():
     # === 生成带时间戳的备份文件名 ===
     global BACKUP_DIR
