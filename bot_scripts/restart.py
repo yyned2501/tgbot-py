8c0ca29fa3c9ea778066591d2a5f9f2ceb037425
Yy <yyned2501@vip.qq.com>
2025-05-26 16:13:49 +0800

    bug fix

--- bot_scripts/restart.py
+++ bot_scripts/restart.py
@@ -2,9 +2,12 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 from config.config import MY_TGID
 import subprocess
+from libs.log import logger
 
 
 @Client.on_message(filters.chat(MY_TGID) & filters.command("restart"))
 async def restart_tg_bot(client: Client, message: Message):
+    logger.info("Restarting the bot...")
+    await message.reply("重启机器人中，请稍候...")
     command = ["docker", "restart", " $(hostname)"]
     subprocess.run(command)
