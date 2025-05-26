edaa3bb94e534f694e8b197b88e7c3f8e74da8ee
Yy <yyned2501@vip.qq.com>
2025-05-26 17:09:30 +0800

    修改更新逻辑

--- bot_scripts/restart.py
+++ bot_scripts/restart.py
@@ -5,9 +5,13 @@ import subprocess
 from libs.log import logger
 
 
-@Client.on_message(filters.chat(MY_TGID) & filters.command("restart"))
+@Client.on_message(filters.chat(MY_TGID) & filters.command("update"))
 async def restart_tg_bot(client: Client, message: Message):
-    logger.info("Restarting the bot...")
-    await message.reply("重启机器人中，请稍候...")
+    await message.reply("检测更新中...")
     command = ["bash", "update"]
-    subprocess.run(command)
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        logger.info("未检测到更新")
+    else:
+        logger.info(result.stdout)
+        logger.error(result.stderr)
