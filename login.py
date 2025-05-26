e031f9dcf1ddabb54075b0db2d09a4b4ee4ec493
Yy <yyned2501@vip.qq.com>
2025-05-26 14:46:36 +0800

    测试botapp

--- login.py
+++ login.py
@@ -4,27 +4,30 @@ from libs.log import logger
 from config.config import PT_GROUP_ID
 from pyrogram import Client
 from app import system_version_get
-from config.config import API_HASH, API_ID,BOT_TOKEN_TEST,proxy_set
+from config.config import API_HASH, API_ID, BOT_TOKEN, proxy_set
 
 
-
-if proxy_set['proxy_enable'] == True:
-    proxy = proxy_set['proxy']
+if proxy_set["proxy_enable"] == True:
+    proxy = proxy_set["proxy"]
 else:
     proxy = None
-async def main():
 
+
+async def main():
     user_app = Client(
-        "sessions/user_account",
+        "sessions/user_account", api_id=API_ID, api_hash=API_HASH, proxy=proxy
+    )
+    bot_app = Client(
+        "sessions/bot_account",
         api_id=API_ID,
         api_hash=API_HASH,
-        proxy=proxy
+        bot_token=BOT_TOKEN,
+        proxy=proxy,
     )
-
-
-    async with user_app:
+    id = await user_app.me.id
+    async with bot_app:
         re_mess = await system_version_get()
-        await user_app.send_message(PT_GROUP_ID['BOT_MESSAGE_CHAT'], f"{re_mess} \n注意本次为首次登录")
+        await bot_app.send_message(id, f"{re_mess} \n注意本次为首次登录")
         logger.info("Mytgbot首次登录成功，登录信息创建成功")
         command = ["supervisorctl", "start", "main"]
         result = subprocess.run(command, capture_output=True, text=True)
