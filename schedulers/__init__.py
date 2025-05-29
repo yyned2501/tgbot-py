ab97a3ba9e2d66177dc1a85db1970d464023971b
Yy <yyned2501@vip.qq.com>
2025-05-29 17:26:06 +0800

    添加调度器作业启动日志记录

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from libs.state import state_manager
+from libs.log import logger
 
 scheduler = AsyncIOScheduler()
 
@@ -16,4 +17,5 @@ scheduler_jobs = {"autofire": zhuque_autofire_firsttimeget}
 async def start_scheduler():
     for job in (schedulers := state_manager.get_section("scheduler", {})):
         if schedulers[job] == "on" and job in scheduler_jobs.keys():
+            logger.info(f"Starting scheduler job: {job}")
             await scheduler_jobs[job]()
